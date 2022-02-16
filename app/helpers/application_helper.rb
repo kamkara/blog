45d0655ffd260a0e5f2b05ecf527d089b0e672a5
kamkara <lnclass.kamkara@gmail.com>
2022-02-16 15:24:17 +0000

    add onglet message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+#Onglet Title
+def title
+ base_title = "Blog kamkar"
+  if @title.nil?
+  base_title
+  else
+  "#{@title} | #{base_title}"
+  end
+end
+
+#site description
+def description
+  "Rails dev ......"
+end
+
+def keywords
+  "learn rails, rails app"
+end
 end
