5b85e828d02ada31da5b7650fdb10c6f6f8ee4d9
kamkara <lnclass.kamkara@gmail.com>
2022-02-16 13:40:51 +0000

    build blog SPA

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to:"posts#index"
+  resources :posts
+  get '/notice', to: 'posts#clear_message'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
